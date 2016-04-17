f41a284560bceb358d48cb741d2411f702741905
Sanjay Menakuru <balasanjay@gmail.com>
2016-04-16 23:10:39 -0700

    Proxy server that automatically fetches/renews certs.

---
+++ server/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/jonathanwei/wile"
+)
+
+func main() {
+	var (
+		backendsFlag = flag.String("backends", "", "Comma-separated list of backends. Each backend is of the form <short-name>:<url>")
+		hostsFlag    = flag.String("hosts", "", "Comma-separated list of hosts to serve and their correspondin backend. Each host is of the form <host>:<backend>")
+		development  = flag.Bool("insecure_development_mode", false, "True iff the server should run in an insecure development mode.")
+		acmeEndpoint = flag.String("acme", "https://acme-staging.api.letsencrypt.org/directory", "The ACME server to sign certs.")
+		acmeEmail    = flag.String("email", "", "The email to use when registering with acme.")
+		certPath     = flag.String("cert_path", "$HOME/.config/wile/wile.json", "The path to cache certs.")
+	)
+
+	flag.Parse()
+
+	backends := parseBackendSpecs(*backendsFlag)
+	hosts := parseHostSpecs(*hostsFlag, backends)
+
+	var domains []string
+	for h := range hosts {
+		domains = append(domains, h)
+	}
+
+	wileCfg := &wile.Config{
+		APIEndpoint:    *acmeEndpoint,
+		Email:          *acmeEmail,
+		Path:           os.ExpandEnv(*certPath),
+		InitialDomains: domains,
+	}
+
+	run(backends, hosts, *development, wileCfg)
+}
+
+func parseBackendSpecs(specs string) map[string]*url.URL {
+	backends := make(map[string]*url.URL)
+	for _, spec := range strings.Split(specs, ",") {
+		fatal := func(msg string) {
+			log.Fatalf("Invalid backend spec %q, %s", spec, msg)
+		}
+
+		idx := strings.Index(spec, ":")
+		if idx == -1 {
+			fatal("missing ':'")
+		}
+
+		name := spec[:idx]
+		ustr := spec[idx+1:]
+
+		if len(name) == 0 {
+			fatal("empty name not allowed")
+		}
+
+		if len(ustr) == 0 {
+			fatal("empty url not allowed")
+		}
+
+		if _, ok := backends[name]; ok {
+			fatal("duplicate backend name")
+		}
+
+		u, err := url.Parse(ustr)
+		if err != nil {
+			fatal(fmt.Sprintf("couldn't parse url: %v", err))
+		}
+
+		backends[name] = u
+	}
+
+	return backends
+}
+
+func parseHostSpecs(specs string, backends map[string]*url.URL) map[string]string {
+	hosts := make(map[string]string)
+	for _, spec := range strings.Split(specs, ",") {
+		fatal := func(msg string) {
+			log.Fatalf("Invalid host spec %q, %s", spec, msg)
+		}
+
+		idx := strings.Index(spec, ":")
+		if idx == -1 {
+			fatal("missing ':'")
+		}
+
+		host := spec[:idx]
+		backend := spec[idx+1:]
+
+		if len(host) == 0 {
+			fatal("empty host not allowed")
+		}
+
+		if _, ok := hosts[host]; ok {
+			fatal("duplicate host not allowed")
+		}
+
+		if _, ok := backends[backend]; !ok {
+			fatal("unknown backend")
+		}
+
+		hosts[host] = backend
+	}
+
+	return hosts
+}
---
+++ server/server.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"crypto/tls"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/golang/glog"
+	"github.com/jonathanwei/wile"
+	"github.com/unrolled/secure"
+)
+
+func run(backends map[string]*url.URL, hosts map[string]string, isDev bool, wileCfg *wile.Config) {
+	wileClient, err := wile.NewClient(wileCfg)
+	if err != nil {
+		glog.Fatalf("Got error creating wile client: %v", err)
+	}
+
+	go httpServer(isDev)
+	httpsServer(backends, hosts, isDev, wileClient)
+}
+
+type proxy struct {
+	handlers map[string]http.Handler
+}
+
+func newProxy(backends map[string]*url.URL, hosts map[string]string) *proxy {
+	handlers := make(map[string]http.Handler)
+
+	for host, backendName := range hosts {
+		backendURL := backends[backendName]
+		handlers[host] = httputil.NewSingleHostReverseProxy(backendURL)
+	}
+
+	return &proxy{
+		handlers: handlers,
+	}
+}
+
+func (p *proxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	h, ok := p.handlers[req.Host]
+	if !ok {
+		glog.Infof("Got request for non-existent hostname %q", req.Host)
+		http.NotFound(rw, req)
+		return
+	}
+
+	h.ServeHTTP(rw, req)
+}
+
+func httpsServer(backends map[string]*url.URL, hosts map[string]string, isDev bool, wileClient *wile.Client) {
+	handler := newProxy(backends, hosts)
+	server := &http.Server{
+		Handler: securify(isDev, handler),
+		TLSConfig: &tls.Config{
+			GetCertificate: wileClient.GetCertificate,
+			MinVersion:     tls.VersionTLS12,
+		},
+	}
+	glog.Fatal(server.ListenAndServeTLS("", ""))
+}
+
+func httpServer(isDev bool) {
+	handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		u := &url.URL{
+			Scheme:   "https",
+			Host:     req.Host,
+			Path:     req.URL.Path,
+			RawQuery: req.URL.RawQuery,
+		}
+		http.Redirect(rw, req, u.String(), http.StatusMovedPermanently)
+	})
+
+	glog.Fatal(http.ListenAndServe(":http", securify(isDev, handler)))
+}
+
+func securify(isDev bool, handler http.Handler) http.Handler {
+	secureMiddleware := secure.New(secure.Options{
+		STSSeconds:            60 * 60 * 24 * 365, // One year.
+		STSIncludeSubdomains:  true,
+		STSPreload:            true,
+		FrameDeny:             true,
+		ContentTypeNosniff:    true,
+		BrowserXssFilter:      true,
+		ContentSecurityPolicy: "default-src 'self'; object-src 'none'",
+		IsDevelopment:         isDev,
+	})
+	return secureMiddleware.Handler(handler)
+}
